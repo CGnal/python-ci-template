e13f002d73f5f51b107d7c2ae2b4f23175d06715
Github Pages Overwriter <GithubPagesOverwriter@users.noreply.github.com>
2022-02-21 22:46:39 +0100

    [MISC] Fixes to initial parametrization

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -23,18 +23,18 @@ config.read(os.path.join(BASE_FOLDER, 'setup.cfg'))
 # -- Project information -----------------------------------------------------
 
 project = config['sphinx']['name']
+package_dir = config["sphinx"]["package_dir"]
 author = config['metadata']['author']
 copyright = str(date.today().year) + ', ' + author
-package_dir = config["sphinx"]["package_dir"]
 
-sys.path.insert(0, os.path.join(BASE_FOLDER, package_dir))
+sys.path.insert(0, os.path.join(BASE_FOLDER))
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'm2r2']
+extensions = ['sphinx.ext.autodoc', 'm2r2', 'sphinx.ext.githubpages']
 autoclass_content = 'both'
 
 # Add any paths that contain templates here, relative to this directory.
